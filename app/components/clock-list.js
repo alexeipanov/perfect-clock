ecb1b0c10892fa40eaa08d34a9be93f71fddb765
Alexei Panov <windstarter@gmail.com>
2017-10-12 23:54:00 +0300

    add timeline and hours labels

--- app/components/clock-list.js
+++ app/components/clock-list.js
@@ -1,12 +1,24 @@
 import Ember from 'ember';
 import { inject as service } from '@ember/service';
+import moment from 'moment';
 
 export default Ember.Component.extend({
   cities: service('cities'),
-  filtered: Ember.computed('cities', function() {
+  hours: [],
+  selected: Ember.computed('cities', function() {
     let defaults = this.get('cities').defaultIDs;
     return this.get('cities').items.filter(function(item) {
       return defaults.some(elem => elem === item.id)
     });
-  })
+  }),
+  didInsertElement(...params) {
+    this._super(...params);
+    let startTime = moment().subtract(18, 'hours');
+    let time, isWorking;
+    for (let i = 0; i < 36; i++) {
+      time = startTime.add(1, 'hours');
+      isWorking = time.hour() >= 8 && time.hour() <= 19
+      this.get('hours').addObject({time: time.unix(), working: isWorking});
+    }
+  }
 });
