93763445ab9e1364177322066105d38bef8c50b1
Alexei Panov <windstarter@gmail.com>
2017-10-15 00:27:48 +0300

    add utc time offset to cities data

--- app/services/cities.js
+++ app/services/cities.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Service.extend({
   items: null,
-  defaultIDs: null,
+  defaultIds: null,
 
   init() {
     this._super(...arguments);
@@ -10,44 +10,44 @@ export default Ember.Service.extend({
       {
         id: 1,
         name: 'Melbourne',
+        utc: 10,
       },
       {
         id: 2,
         name: 'Singapore',
+        utc: 8,
       },
       {
         id: 3,
         name: 'Warsaw',
+        utc: 1,
       },
       {
         id: 4,
         name: 'Moscow',
+        utc: 3,
       },
       {
         id: 5,
         name: 'London',
+        utc: 0,
       },
       {
         id: 6,
         name: 'New York',
+        utc: -5,
       },
       {
         id: 7,
         name: 'San Francisco',
+        utc: -8,
       },
       {
         id: 8,
         name: 'Kiev',
+        utc: 2,
       },
     ]);
     this.set('defaultIds', [1, 3, 7]);
   }
 });
-
-
-
-
-
-
-
-
