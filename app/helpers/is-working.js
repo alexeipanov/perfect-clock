41e3c522595e176eb36d6f595fa2dced58547783
Aleks Witko <aleks.witko@gmail.com>
2017-10-18 22:08:56 +1000

    style updates

--- app/helpers/is-working.js
+++ app/helpers/is-working.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 import moment from 'moment';
 
 export function isWorking(params, { source, offset }) {
-  return moment.unix(source).utc().utcOffset(offset).hour() >= 8 && moment.unix(source).utc().utcOffset(offset).hour() <= 19;
+  return moment.unix(source).utc().utcOffset(offset).hour() >= 9 && moment.unix(source).utc().utcOffset(offset).hour() <= 17;
 }
 
 export default Ember.Helper.helper(isWorking);
