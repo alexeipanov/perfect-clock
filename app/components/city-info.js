cd3e2aaba16c90b1ac1134a495a2f80bf31d0456
Alexei Panov <windstarter@gmail.com>
2017-10-30 15:25:25 +0300

    remove unused fadein effects

--- app/components/city-info.js
+++ app/components/city-info.js
@@ -2,17 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   classNames: ['info'],
-  // classNameBindings: ['isUpdated'],
-  isUpdated: false,
-  didInsertElement() {
-    this.get('element').addEventListener('animationend', this.removeClass.bind(this), false);
-  },
-  didUpdateAttrs() {
-    this.set('isUpdated', true);
-  },
-  removeClass() {
-    this.set('isUpdated', false);
-  },
   actions: {
     onTimeClick(duration) {
       this.get('onTimeClick')(duration);
